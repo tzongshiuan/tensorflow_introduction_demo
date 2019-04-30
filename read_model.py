ef31ac2752f09db727c4ca6de76d216f6ca23504
tsunghsuan <tsunghsuanlai@gorilla-technology.com>
2019-04-30 11:36:27 +0800

    Update read_model.py

--- read_model.py
+++ read_model.py
@@ -6,6 +6,21 @@ from io import StringIO
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+
+def get_image(index):
+    return {
+        0: 'mnist/test/0/3.jpg',
+        1: 'mnist/test/0/10.jpg',
+        2: 'mnist/test/1/137.jpg',
+        3: 'mnist/test/1/203.jpg',
+        4: 'mnist/test/2/119.jpg',
+        5: 'mnist/test/2/280.jpg',
+        6: 'mnist/test/7/4423.jpg',
+        7: 'mnist/test/7/8651.jpg',
+        8: 'mnist/test/8/3406.jpg',
+        9: 'mnist/test/8/8661.jpg',
+    }[index]
+
 with tf.Session() as sess:
     ##################################################
     # load model #
@@ -29,9 +44,11 @@ with tf.Session() as sess:
     # for i in range(24):
         # img = cv2.imread('images/1/%d.jpg' % i)
     # img = cv2.imread('images/0/3.jpg')
-    img = cv2.imread('mnist/test/3/44.jpg', 0)
+    # img = cv2.imread('mnist/test/0/3.jpg', 0)
 
-    # 辨識影像，並印出結果
-    result = sess.run(y, feed_dict={x: img.reshape((-1, 28 * 28))})
-    # result = sess.run(y, feed_dict={x: img.reshape((-1, 256 * 256 * 3))})
-    print(result)
+    for i in range(10):
+        # 辨識影像，並印出結果
+        img = cv2.imread(get_image(i), 0)
+        result = sess.run(y, feed_dict={x: img.reshape((-1, 28 * 28))})
+        # result = sess.run(y, feed_dict={x: img.reshape((-1, 256 * 256 * 3))})
+        print(result)
